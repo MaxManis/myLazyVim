b788cd3a0b9edd60a21944372def7acba4a90fad
Max Bovtun <maxbovtun@gmail.com>
2024-03-26 11:07:19 +0200

    my config added and edited

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,6 +9,70 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- Typescript options:
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        ---@type lspconfig.options.tsserver
+        tsserver = {
+          keys = {
+            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+          },
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      },
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+  { "jose-elias-alvarez/typescript.nvim" },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "js-debug-adapter")
+    end,
+  },
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
@@ -214,53 +278,105 @@ return {
 
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  -- then: setup supertab in cmp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
+  -- -- then: setup supertab in cmp
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   config = function()
+  --     local cmp = require("cmp")
+  --     cmp.setup({
+  --       mapping = {
+  --         -- If nothing is selected (including preselections) add a newline as usual.
+  --         -- If something has explicitly been selected by the user, select it.
+  --         ["<Tab>"] = function(fallback)
+  --           -- Don't block <CR> if signature help is active
+  --           -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13
+  --           if
+  --             not cmp.visible()
+  --             or not cmp.get_selected_entry()
+  --             or cmp.get_selected_entry().source.name == "nvim_lsp_signature_help"
+  --           then
+  --             fallback()
+  --           else
+  --             cmp.confirm({
+  --               -- Replace word if completing in the middle of a word
+  --               -- https://github.com/hrsh7th/nvim-cmp/issues/664
+  --               behavior = cmp.ConfirmBehavior.Replace,
+  --               -- Don't select first item on CR if nothing was selected
+  --               select = false,
+  --             })
+  --           end
+  --         end,
+  --         ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --           -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+  --           if cmp.visible() then
+  --             local entry = cmp.get_selected_entry()
+  --             if not entry then
+  --               cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  --             else
+  --               cmp.complete()
+  --             end
+  --           else
+  --             fallback()
+  --           end
+  --         end, { "i", "s", "c" }),
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-emoji",
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local has_words_before = function()
+  --       unpack = unpack or table.unpack
+  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --     end
+  --
+  --     local luasnip = require("luasnip")
+  --     local cmp = require("cmp")
+  --
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       -- ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.confirm({ select = true })
+  --           -- local entry = cmp.get_selected_entry()
+  --           -- if not entry then
+  --           --   cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  --           -- else
+  --           --   cmp.confirm()
+  --           -- end
+  --           -- cmp.select_next_item()
+  --           --       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+  --           --       -- this way you will only jump inside the snippet region
+  --           --     elseif luasnip.expand_or_jumpable() then
+  --           --       luasnip.expand_or_jump()
+  --         elseif has_words_before() then
+  --           cmp.complete()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       --   ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --       --     if cmp.visible() then
+  --       --       cmp.select_prev_item()
+  --       --     elseif luasnip.jumpable(-1) then
+  --       --       luasnip.jump(-1)
+  --       --     else
+  --       --       fallback()
+  --       --     end
+  --       --   end, { "i", "s" }),
+  --     })
+  --   end,
+  -- },
 }
